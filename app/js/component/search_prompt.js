91cfac94956a9d0e8c89da562f2182230c29c154
Pascal Hartig <phartig@rdrei.net>
2013-07-06 02:11:30 +0200

    Restyled to have scrollable columns

--- app/js/component/search_prompt.js
+++ app/js/component/search_prompt.js
@@ -21,7 +21,8 @@ define(function (require) {
   function searchPrompt() {
     this.defaultAttrs({
       selector: '#td-search-modal',
-      saveSelector: '#td-search-modal form'
+      saveSelector: '#td-search-modal form',
+      deleteSelector: '#td-search-modal .td-delete-btn'
     });
 
     // There can only be one search prompt at the time, so it's save
@@ -43,9 +44,17 @@ define(function (require) {
       });
     };
 
+    this.delete = function (ev) {
+      ev.preventDefault();
+      this.trigger(origin, 'uiColumnDeletionRequested');
+    };
+
     this.after('initialize', function () {
       this.on(document, 'uiShowSearchPrompt', this.show);
 
+      this.on('click', {
+        deleteSelector: this.delete
+      });
       this.on('submit', this.save);
     });
   }
